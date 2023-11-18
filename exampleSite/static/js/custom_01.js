ae1bf1b1d15001556a44249105f5505dd7a358ef
gkabas <46166787+gkabas@users.noreply.github.com>
2023-11-18 21:07:30 +0100

    Update custom_01.js

--- exampleSite/static/js/custom_01.js
+++ exampleSite/static/js/custom_01.js
@@ -1,3 +1,21 @@
 const yourCustomFunction01 = (message) => {
   console.log(message);
 };
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
