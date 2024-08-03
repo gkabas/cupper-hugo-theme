a0aae17ed164f7d5055f5ad793c679d9acad97b0
gkabas <46166787+gkabas@users.noreply.github.com>
2024-08-03 19:59:48 +0200

    Update custom_01.js

--- exampleSite/static/js/custom_01.js
+++ exampleSite/static/js/custom_01.js
@@ -54,7 +54,7 @@ function pdf5() {
     }
 
 function ssrn5() {
-    window.open("https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3280437", "_blank");
+    window.open("https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4915102", "_blank");
     }
 
 function dis1() {
