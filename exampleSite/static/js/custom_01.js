cb800583ac4a50e4f132e804292f39dce1d90b26
gkabas <46166787+gkabas@users.noreply.github.com>
2024-08-03 17:58:22 +0200

    Update custom_01.js

--- exampleSite/static/js/custom_01.js
+++ exampleSite/static/js/custom_01.js
@@ -199,12 +199,13 @@ function fig5() {
   }
 }
 
-let slideIndex = [1,1,1,1];
+let slideIndex = [1,1,1,1,1];
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
+showSlides(1, 4);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
